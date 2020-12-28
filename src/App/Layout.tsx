686af77397d56520dcd4de35fbef45a093d330d0
TokiyaHorikawa <ayikot4423@gmail.com>
2020-12-28 18:04:28 +0900

    feat: add React Router

---
+++ src/App/Layout.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import BaseRoute from 'App/Router';
+
+const Layout: React.FC = () => {
+  return <BaseRoute />;
+};
+
+export default Layout;
---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h2>ホーム</h2>
+      <p>割り勘自動計算</p>
+    </div>
+  );
+};
+export default Home;
