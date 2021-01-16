4df763188d7a22593bf3ce83ec62dbf5c4c91402
TokiyaHorikawa <ayikot4423@gmail.com>
2021-01-16 21:07:50 +0900

    feat: make charge input funtion

--- src/App/Layout.tsx
+++ src/App/Layout.tsx
@@ -2,7 +2,21 @@ import React from 'react';
 import BaseRoute from 'App/Router';
 
 const Layout: React.FC = () => {
-  return <BaseRoute />;
+  const LoginedLayout: React.FC = ({ children }) => {
+    return (
+      <div className="app">
+        <div className="header w-full h-12 flex justify-center items-center bg-white border-b">
+          <h1>ワリ・カン</h1>
+        </div>
+        <div className="main">{children}</div>
+      </div>
+    );
+  };
+  return (
+    <LoginedLayout>
+      <BaseRoute />
+    </LoginedLayout>
+  );
 };
 
 export default Layout;
